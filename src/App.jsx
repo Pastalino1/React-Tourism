af160ad3e7c7e2020ee35386d8de609d221c8e71
Pastalino1 <113046699+Pastalino1@users.noreply.github.com>
2024-07-16 19:56:34 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar.jsx"
+function App() {
+
+  return(
+    <>
+    <Navbar/>
+    </>
+
+  );
+
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar(){
+  return (
+    <nav className="navbar">
+      <div className="logo">LOGO</div>
+      <div className="nav-links">
+        <a href="#home1" className="nav-link">Home</a>
+        <a href="#home2" className="nav-link">About Us</a>
+        <a href="#home3" className="nav-link">Explore</a>
+        <a href="#home4" className="nav-link">Contact</a>
+        <button class="button-6" role="button">Button 6</button>
+       </div>
+    </nav>
+  );
+
+    
+
+}
+export default Navbar;
